9f2f76a815f71f9296d4d9f0147556a5fee8344c
Jessica <aleejess@gmail.com>
2022-07-29 22:23:20 -0500

    feat: add Countdown and disable button

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,19 +1,19 @@
 import './index.css';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const Header = () => (
   <div className="header__Container">
     <div className="header__Title">
-      <h1>Product Store</h1>
+      <h2 className="header_h2">Product Store</h2>
     </div>
-    <div className="header__menuWraper">
+    {/* <div className="header__menuWraper">
       <Link className="header__linkhome" to="/">
         Home
       </Link>
       <Link className="header__linkAbout" to="/about">
         About
       </Link>
-    </div>
+    </div> */}
   </div>
 );
 
--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,23 +1,60 @@
 import './index.css';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-// import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
-const ProductCard = ({ main }) => (
-  <div className="productCard__container">
-    <div className="productCard__imageWrapper">
-      <img className="productCard__image" src={main.image} alt={main.title} />
-    </div>
-    <div className="ProductCard__Text">
-      <span><b>{main.title}</b></span>
-    </div>
-    <Link className="product-link" to={`/products/${main.id}`}>
-      <div className="ProductCard__button">
-        <button type="submit" className="productCard__button"> Go To Detail </button>
+const ProductCard = ({ main }) => {
+  const [minutes, setMinutes] = useState(Math.floor(Math.random() * 3));
+  const [seconds, setSeconds] = useState(Math.floor(Math.random() * 59));
+  const [stopbutton, setStopbutton] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      clearInterval(interval);
+      if (seconds === 0) {
+        if (minutes !== 0) {
+          setSeconds(59);
+          setMinutes(minutes - 1);
+        } else {
+          setStopbutton(true);
+        }
+      } else {
+        setSeconds(seconds - 1);
+      }
+    }, 1000);
+  }, [seconds]);
+  const timerMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  const timerSeconds = seconds < 10 ? `0${seconds}` : seconds;
+
+  return (
+    <div className="productCard__container">
+      <div className="productCard__imageWrapper">
+        <img className="productCard__image" src={main.image} alt={main.title} />
       </div>
-    </Link>
-  </div>
-);
+      <div>
+        {timerMinutes}:{timerSeconds}
+      </div>
+      <div className="ProductCard__Text">
+        <span><b>{main.title}</b></span>
+      </div>
+      {
+      stopbutton
+        ? (
+          <div className="ProductCard__button">
+            <button disable type="submit" className="productCard__button"> Go To Detail </button>
+          </div>
+        )
+        : (
+          <Link className="product-link" to={`/products/${main.id}`}>
+            <div className="ProductCard__button">
+              <button type="submit" className="productCard__button"> Go To Detail </button>
+            </div>
+          </Link>
+        )
+      }
+    </div>
+  );
+};
 
 ProductCard.propTypes = {
   main: PropTypes.shape(),
